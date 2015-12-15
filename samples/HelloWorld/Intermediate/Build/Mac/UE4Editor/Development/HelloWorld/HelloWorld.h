5d5e93495ce303a9da801a8a16550f36bb6ef996
WooKyoung Noh <wookay.noh@gmail.com>
2015-12-16 04:58:20 +0900

    helloworld

---
+++ samples/HelloWorld/Intermediate/Build/Mac/UE4Editor/Development/HelloWorld/HelloWorld.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_HELLOWORLD_H__
+#define __AUTO_HELLOWORLD_H__
+//Last Write: 12/15/2015 7:35:50 PM +00:00
+#include "/Users/loser/work/unrealcat/samples/HelloWorld/Source/HelloWorld/HelloWorld.h"
+#endif//__AUTO_HELLOWORLD_H__
---
+++ samples/HelloWorld/Source/HelloWorld/HelloWorldGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "HelloWorldGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HELLOWORLD_API AHelloWorldGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
